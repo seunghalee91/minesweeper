746ae15029346967373a67d52b4a8c7a07854396
lsh <lsh>
2020-05-19 11:01:23 +0900

    Minemap,Mineitem class 생성

---
+++ Minesweeper/MineItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    public class MineItem
+    {
+        public bool IsBomb
+        {
+            get;
+            set;
+        }
+        public int NearBombsCount
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            if (IsBomb)
+            {
+                return "*";
+            }
+            return NearBombsCount.ToString();
+        }
+    }
+}
--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -10,23 +10,36 @@ namespace Minesweeper
     {
         static void Main(string[] args)
         {
-            string[,] arr = new string[5, 5] 
-            { 
-                { "0", "1", "1", "1", "0" }, 
-                { "0", "1", "*", "1", "0" }, 
-                { "1","3","3","2","0"},
-                {"1","*","*","1","0" }, 
-                { "1", "2", "2", "1", "0" }
+            //string[,] arr = new MineItem[5, 5] 
+            //{ 
+            //    { "0", "1", "1", "1", "0" }, 
+            //    { "0", "1", "*", "1", "0" }, 
+            //    { "1","3","3","2","0"},
+            //    {"1","*","*","1","0" }, 
+            //    { "1", "2", "2", "1", "0" }
+            //};
+
+
+            MineItem[,] arr = new MineItem[5, 5]
+            {
+                { new MineItem { IsBomb = false, NearBombsCount = 0 }, new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 0 } },
+                { new MineItem { IsBomb = false, NearBombsCount = 0 }, new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = true, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 0 } },
+                { new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 3 }, new MineItem { IsBomb = false, NearBombsCount = 3 }, new MineItem { IsBomb = false, NearBombsCount = 2 }, new MineItem { IsBomb = false, NearBombsCount = 0 } },
+                { new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = true, NearBombsCount = 1 }, new MineItem { IsBomb = true, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 0 } },
+                { new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 2 }, new MineItem { IsBomb = false, NearBombsCount = 2 }, new MineItem { IsBomb = false, NearBombsCount = 1 }, new MineItem { IsBomb = false, NearBombsCount = 0 } },
             };
 
-            for(int i=0;i<5;i++)
+
+            for (int i = 0; i < 5; i++)
             {
-                for(int j=0;j<5;j++)
+                for (int j = 0; j < 5; j++)
                 {
+                    //Console.Write("{0} ", arr[i, j]);
                     Console.Write("{0} ", arr[i, j]);
                 }
                 Console.WriteLine();
             }
+
 
             Console.ReadKey();
         }
